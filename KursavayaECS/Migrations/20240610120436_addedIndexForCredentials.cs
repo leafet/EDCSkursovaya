542455834b2d99ec99168e5d5a32ed86f98587e4
leafet <zakhar23042004@rtural.ru>
2024-06-10 20:07:38 +0500

    [Feature] addMigrationWitnIndex

---
+++ KursavayaECS/Migrations/20240610120436_addedIndexForCredentials.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KursavayaECS.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedIndexForCredentials : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email_ID",
+                table: "Users",
+                columns: new[] { "Email", "ID" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email_ID",
+                table: "Users");
+        }
+    }
+}
